5b1de61d56613af08220f0565d13dbf1fb83a77f
ElsongMaster <elsongmaster@gmail.com>
2021-04-30 09:59:15 +0200

    exo 2

---
+++ exo2/src/js/main.js
@@ -0,0 +1,49 @@
+// déclaration , concaténation et addition
+let str1 = "J'aime les tartines et j'en mange";
+console.log(str1);
+
+let str2 = "4 par jours";
+
+console.log(`${str1} ${str2}`);
+
+let myInt1 = 5;
+
+let myInt2 = 35;
+
+console.log(myInt1 + myInt2);
+
+str1 = +myInt1;
+
+console.log(str1);
+
+// Reaffectation
+
+let helloStr = "Salut tout le monde";
+
+console.log(helloStr);
+
+let goodbyeStr = "Aurevoir !";
+
+console.log(goodbyeStr);
+
+helloStr = goodbyeStr;
+
+console.log(helloStr);
+
+// opérateurs
+
+let myVar1 = 4;
+
+let myVar2 = "6";
+
+console.log("myVar1 > myVar2: " + (myVar1 > myVar2));
+
+console.log("myVar1 < myVar2: " + (myVar1 < myVar2));
+
+console.log("myVar1 == myVar2: " + (myVar1 == myVar2));
+
+console.log("myVar1 != myVar2: " + (myVar1 != myVar2));
+
+console.log("myVar1 === myVar2: " + (myVar1 === myVar2));
+
+console.log("myVar1 !== myVar2: " + (myVar1 !== myVar2));
