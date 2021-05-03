9644093a334cadffcef94c607410597680c40deb
ElsongMaster <elsongmaster@gmail.com>
2021-05-03 09:37:53 +0200

    exo 6

---
+++ exo6/public/js/main.js
@@ -0,0 +1,47 @@
+//1.
+let nombre = 8.56;
+
+//2.
+
+Math.round(nombre);
+
+//3.
+
+Math.floor(nombre);
+
+//4.
+
+Math.ceil(nombre);
+
+//5.
+
+Math.trunc(nombre);
+
+//6.
+
+console.log(Math.random());
+
+//7.
+console.log(Math.random() * 100);
+
+//8.
+
+console.log(Math.round(Math.random() * 100));
+
+//9.
+
+console.log(Math.pow(8, 2));
+
+//10.
+console.log(Math.sqrt(9));
+
+//11.
+
+console.log(Math.abs(-1));
+
+//12.
+
+console.log(Math.max(-2, 1000, 8, 57));
+
+//13.
+console.log(Math.min(-2, 1000, 8, 57));
